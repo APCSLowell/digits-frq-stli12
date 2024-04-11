040e1d1fffdd04aa8b2a1df82f5c011fae0bb6cf
stli12 <143028001+stli12@users.noreply.github.com>
2024-04-11 15:59:12 -0700

    Update Digits.java

--- src/main/java/Digits.java
+++ src/main/java/Digits.java
@@ -20,7 +20,7 @@ public class Digits
 	public boolean isStrictlyIncreasing()
 	{ /* to be implemented in part (b) */
 		for(int i = 0; i < digits.size()-1; i ++){
-	if(digits.get(i) >= digits.get(i+1))
+	if(digits.get(i).compareTo( digits.get(i+1)) < 0)
 		return false;
 
 		}
