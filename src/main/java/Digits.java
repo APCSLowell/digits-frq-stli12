ca375890d34865361fda2c5815284a1aa9cbd748
stli12 <143028001+stli12@users.noreply.github.com>
2024-04-11 15:55:24 -0700

    Update Digits.java

--- src/main/java/Digits.java
+++ src/main/java/Digits.java
@@ -7,12 +7,24 @@ public class Digits
 
 	public Digits(int num)
 	{ /* to be implemented in part (a) */ 
-	    
+	   digitList = new ArrayList<Integer>;
+	if(num == 0);
+		digitList.add(new Integer(0));
+
+	while(num > 0){
+		digitList.add(new Integer(num%10);
+		num = num/10;
+	}
 	}
 
 	public boolean isStrictlyIncreasing()
 	{ /* to be implemented in part (b) */
-		
+		for(int i = 0; i < digits.size()-1; i ++){
+	if(digits.get(i) >= digits.get(i+1))
+		return false;
+
+		}
+	return true;
 
 	}
 	
